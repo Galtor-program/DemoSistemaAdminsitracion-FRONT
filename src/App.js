59db5c7ea5e329552fa2caf0387b217544d4c8b5
Galtor-program <felipetorog@gmail.com>
2024-03-20 17:12:16 -0300

    Ultimo Commit con fondo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ListadoEmpleados from "./empleados/ListadoEmpleados";
+import Navegacion from "./plantilla/Navegacion";
+import AgregarEmpleado from "./empleados/AgregarEmpleado";
+import EditarEmpleado from "./empleados/EditarEmpleado";
+import './css/App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="app">  
+   <div className="container">
+      <BrowserRouter>
+        <Navegacion />
+        <Routes>
+          <Route exact path="/" element={<ListadoEmpleados />}/>
+          <Route exact path="/agregar" element={<AgregarEmpleado />}/>
+          <Route exact path="/editar/:id" element={<EditarEmpleado />}/>
+        </Routes>
+      </BrowserRouter>
+   </div>
+  </div> 
   );
 }
 
